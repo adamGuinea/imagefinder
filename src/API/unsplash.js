9444d18f10b7f9c27103c42aef74ff1132b9c893
adamGuinea <adamguinea22@gmail.com>
2019-08-09 14:06:39 +1000

    separated prod/dev environments

--- src/API/unsplash.js
+++ src/API/unsplash.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
-const API_KEY = process.env.REACT_APP_API_KEY;
-
+import axios from "axios";
+import keys from "../config/keys";
 
 export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: API_KEY
-    }
-})
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: keys.apiKey
+  }
+});
