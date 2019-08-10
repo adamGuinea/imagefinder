c6597f5743cd3a73dd7be0e70795c0066b56bf07
adamGuinea <adamguinea22@gmail.com>
2019-08-10 16:36:52 +1000

    defined layout of sections, began features section

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,14 @@ const App = () => {
       <div className='sidebar'>sidebar</div>
       <header className='header'>header</header>
       <div className='realtors'>top realtors</div>
-      <section className='features'>features</section>
+      <section className='features'>
+        <div className='feature'>feature 1</div>
+        <div className='feature'>feature 2</div>
+        <div className='feature'>feature 3</div>
+        <div className='feature'>feature 4</div>
+        <div className='feature'>feature 5</div>
+        <div className='feature'>feature 6</div>
+      </section>
       <div className='story__pictures'>Pictures</div>
       <div className='story__content'>Content</div>
       <section className='homes'>homes</section>
