8ef8d01adcbf5d2b64d916ade6d2b14c7498979e
adamGuinea <adamguinea22@gmail.com>
2019-08-10 15:13:22 +1000

    defined grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const App = () => {
-  return <body className='container'>hi</body>;
+  return (
+    <div className='container'>
+      <div className='sidebar'>sidebar</div>
+      <header className='header'>header</header>
+      <div className='realtors'>top realtors</div>
+      <section className='features'>features</section>
+      <div className='story__pictures'>Pictures</div>
+      <div className='story__content'>Content</div>
+      <section className='homes'>homes</section>
+      <section className='gallery'>gallery</section>
+      <footer className='footer'>footer</footer>
+    </div>
+  );
 };
 
 export default App;
